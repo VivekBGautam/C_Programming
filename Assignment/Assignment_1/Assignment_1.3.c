fe116a5ce2b3a49eb08c815fc2aceca581db48cd
Vivek Gautam <vivekbgautam@gmail.com>
2025-11-30 12:02:20 +0530

    Update Assignment_1.3.c

--- Assignment/Assignment_1/Assignment_1.3.c
+++ Assignment/Assignment_1/Assignment_1.3.c
@@ -1,23 +1,44 @@
-// Print 5 to 1 on display
 
-#include<stdio.h>
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files
+//
+////////////////////////////////////////////////////////////////////////
 
-void Display()
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name   :   Display5to1
+//  Description     :   Used to display the numbers form 5 to 1 on screen
+//  Input           :   -
+//  output          :   5   4   3   2   1
+//  Author          :   Vivek Bhauraj Gautam
+//  Date            :   17/10/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+void Display5to1()
 {
     int iCnt = 0;
 
     iCnt = 5;
-
-    while(iCnt > 0)
+    while(iCnt >= 1)
     {
-        printf("%d\n",iCnt);
+        printf("%d\t",iCnt);
         iCnt--;
     }
 }
 
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
 int main()
 {
-    Display();
+    Display5to1();
 
     return 0;
 }
